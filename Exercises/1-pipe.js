129987ac725c689905c19e2cbe35290e9ab83e86
MytsV <mytsvictoria@gmail.com>
2021-12-02 20:04:24 +0200

    Module finished

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...fns) => {
+  if (fns.find(fn => typeof fn !== 'function') !== undefined) {
+    throw new Error('An argument is not a function');
+  } else {
+    return x => fns.reduce((pr, curr) => curr(pr), x);
+  }
+};
 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,19 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const compose = (...fns) => {
+  const events = {};
+  const f = x => {
+    try {
+      return fns.reverse().reduce((pr, curr) => curr(pr), x);
+    } catch (e) {
+      events['error'].forEach(fn => fn(e));
+      return undefined;
+    }
+  };
+  f.on = (name, fn) => {
+    events[name] ? events[name].push(fn) : events[name] = [fn];
+  };
+  return f;
+};
 
 module.exports = { compose };
